a777eaefcdd85edf1da7b7421d10072bde301d2b
Priscy <priscillaakwetey03@gmail.com>
2024-07-13 15:01:35 +0000

    adding my first javascript

---
+++ quiz.js
@@ -0,0 +1,17 @@
+function checkAnswer(){
+    const correctAnswer ="4";
+
+    const selectedAnswer = document.querySelector('input[name="quiz"]:checked');
+    const userAnswer = selectedAnswer ? selectedAnswer.value: null;
+
+     const feedBack = document.getElementById('feedback');
+
+    if (userAnswer == correctAnswer){
+        feedBack.textContent = "correct! Well done."
+    }else if(userAnswer != correctAnswer) {
+        feedBack.textContent= "That's incorrect.Try again"
+    }
+}
+
+const SubmitButton = document.getElementById('submit-answer');
+SubmitButton.addEventListener('click', checkAnswer);
