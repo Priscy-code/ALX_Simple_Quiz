47b25c99c88503aa35ad4b814d888edf8832f618
Priscy <priscillaakwetey03@gmail.com>
2024-07-15 10:20:15 +0000

    adding changes

--- quiz.js
+++ quiz.js
@@ -8,7 +8,7 @@ function checkAnswer(){
     if (userAnswer === correctAnswer){
         feedback.textContent = "Correct! Well done.";
     }else if (feedback){
-        feedback.textContent = "Incorrect answer";
+        feedback.textContent = "That's incorrect. Try again";
     }
 }
 const submitButton = document.getElementById('submit-answer');
